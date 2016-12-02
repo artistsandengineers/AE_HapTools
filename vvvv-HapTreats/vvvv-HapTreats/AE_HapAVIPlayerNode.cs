8a0861db0d297ad44b219209a9dd05b455ca1271
Arron Smith <arronsmith@gmail.com>
2016-12-02 13:34:35 +0000

    Empty node for HapAVI player.

--- vvvv-HapTreats/vvvv-HapTreats/AE_HapAVIPlayerNode.cs
+++ vvvv-HapTreats/vvvv-HapTreats/AE_HapAVIPlayerNode.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using VVVV.PluginInterfaces.V1;
 using VVVV.PluginInterfaces.V2;
 
-namespace vvvv_HapTreats
+namespace VVVV.HapTreats.Nodes
 {
-    public class AE_HapAVIPlayerNode
+    [PluginInfo(Name = "HapAVIPlayer", Category = "HapTreats", Version = "1.0", Author = "arron")]
+    public class AE_HapAVIPlayerNode : IPluginEvaluate, IDisposable
     {
+        protected ISpread<bool> FValid;
 
+        public void Evaluate(int spreadMax)
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
     }
 }
