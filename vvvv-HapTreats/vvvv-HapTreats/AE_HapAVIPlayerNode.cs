c497f4229bae2c9659898c2720f8123793a57ce4
Arron Smith <arronsmith@gmail.com>
2016-12-02 12:45:29 +0000

    Added Nuget reference to vvvv.plugininterfaces

--- vvvv-HapTreats/vvvv-HapTreats/AE_HapAVIPlayerNode.cs
+++ vvvv-HapTreats/vvvv-HapTreats/AE_HapAVIPlayerNode.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+
 namespace vvvv_HapTreats
 {
-    public class Class1
+    public class AE_HapAVIPlayerNode
     {
+
     }
 }
